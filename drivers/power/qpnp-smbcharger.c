ee07b6a9980723feafc060086331789654affd3e
ROM-Jeremy <Disciple97414@gmail.com>
2018-06-26 17:13:41 -0400

    Fix compilation errors

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -3783,14 +3783,11 @@ static int smbchg_float_voltage_set(struct smbchg_chip *chip, int vfloat_mv)
 
 	if (rc)
 		dev_err(chip->dev, "Couldn't set float voltage rc = %d\n", rc);
-	else
-		chip->vfloat_mv = vfloat_mv;
-	else
-	{
-		chip->vfloat_mv = vfloat_mv;
-		power_supply_set_voltage_limit(chip->usb_psy,
- 				chip->vfloat_mv * 1000);
- 	}
+	else {
+	    chip->vfloat_mv = vfloat_mv;
+	    power_supply_set_voltage_limit(chip->usb_psy,
+ 		                chip->vfloat_mv * 1000);
+    }
 
 	power_supply_set_voltage_limit(chip->usb_psy,
 				(chip->vfloat_mv * 1000));
